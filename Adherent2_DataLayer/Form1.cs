16c4dd44f448bd32c44bf4eab1748d99e7036e49
William Darcy <user@example.com>
2015-08-09 16:50:13 +0200

    Ajout de commentaires

--- Adherent2_DataLayer/Form1.cs
+++ Adherent2_DataLayer/Form1.cs
@@ -19,6 +19,7 @@ namespace Adherent2_DataLayer
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Récupération de tous les adhérents et affichage dans une listView qui a pour nom listViewAdherents
             AdherentDB adhDB = new AdherentDB();
             List<Adherent> lesAdherents = adhDB.GetAllAdherent();
 
@@ -36,12 +37,16 @@ namespace Adherent2_DataLayer
                 listViewAdherents.Items.Add(listItem);
 
             }
-
+            //Exemple de création d'un nouvel adhérent (avec des données en dur) dans la base. 
+            //On doit récupérer le type d'adhésion pour pouvoir créer l'adhérent.
             TypeAdhesionDB typeDB = new TypeAdhesionDB();
             TypeAdhesion t = typeDB.GetTypeAdhesion(1);
             Adherent a = new Adherent("tom", "tim", "445", "Paris", new DateTime(1980, 5, 6), t);
             adhDB.Save(a);
-            Adherent a1 = adhDB.GetAdherent(14);
+            
+            
+            //Modification de la ville d'un adhérent
+            Adherent a1 = adhDB.GetAdherent(6);
             a1.SetVille("NY");
             adhDB.Save(a1);
 
